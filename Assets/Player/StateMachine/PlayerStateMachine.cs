ca546a8acfba631d04847621807c4aadcddacfa2
VonRiddarn <vonriddarn@gmail.com>
2025-03-29 20:26:51 +0100

    feat: began player state machine component

---
+++ Assets/Player/StateMachine/PlayerStateMachine.cs
@@ -0,0 +1,10 @@
+using FractalPike.Peck.StateManagement;
+using UnityEngine;
+
+namespace FractalPike.Peck.Player
+{
+	public class PlayerStateMachine : StateMachine
+	{
+		// TODO: Do awesome shit here!
+	}	
+}
