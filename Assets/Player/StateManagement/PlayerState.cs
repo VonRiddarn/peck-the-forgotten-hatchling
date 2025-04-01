e32c1a8d766a66ef983da9a45468bff434688509
VonRiddarn <vonriddarn@gmail.com>
2025-04-01 15:08:55 +0200

    feat: added PlayerState as a derivative of state

---
+++ Assets/Player/StateManagement/PlayerState.cs
@@ -0,0 +1,14 @@
+using FractalPike.Peck.StateManagement;
+
+namespace FractalPike.Peck.Player.StateManagement.States
+{
+	public class PlayerState : State
+	{
+		public PlayerState(StateMachine stateMachine, string name) : base(stateMachine, name) {}
+
+		public virtual void HandleJumpInput() {}
+		public virtual void HandleAttackInput() {}
+		public virtual void HandleRollInput() {}
+		public virtual void HandleInteractInput() {}
+	}
+}
