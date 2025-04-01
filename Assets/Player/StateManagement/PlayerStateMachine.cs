14817d4db0d08c65057d3bc208d0b6fafc0d9d6f
VonRiddarn <vonriddarn@gmail.com>
2025-04-01 15:09:06 +0200

    feat: implemented PlayerState into the state machine

--- Assets/Player/StateManagement/PlayerStateMachine.cs
+++ Assets/Player/StateManagement/PlayerStateMachine.cs
@@ -1,3 +1,4 @@
+using FractalPike.Peck.Player.StateManagement.States;
 using FractalPike.Peck.StateManagement;
 using UnityEngine;
 
@@ -5,6 +6,15 @@ namespace FractalPike.Peck.Player.StateManagement
 {
 	public sealed class PlayerStateMachine : StateMachine
 	{
-		// TODO: Do awesome shit here!
+		// Input is in hot path, so we use a field instead of property.
+		/// <summary>Only modify from the appropriate <c>Input manager</c>!</summary>
+		public Vector2 movementDirection = Vector2.zero;
+		
+		// NOTE: We're going for pure speed.
+		// This is using direct casts with no safety. If the state provided is not a PlayerState this WILL crash.
+		public void HandleJumpInput() => ((PlayerState)currentState).HandleJumpInput();
+		public void HandleAttackInput() => ((PlayerState)currentState).HandleAttackInput();
+		public void HandleRollInput() => ((PlayerState)currentState).HandleRollInput();
+		public void HandleInteractInput() => ((PlayerState)currentState).HandleInteractInput();
 	}	
 }
